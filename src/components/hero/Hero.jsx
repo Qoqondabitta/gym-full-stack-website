49758b9bf8dba86e15511e2c8b151513280312ee
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-11-09 02:18:15 +0500

    navbar has been finished and hero has been started Alhamdulillah

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+    return <div className="main-common-style center">
+        <div className="container-common-style center">
+            <h2 className="hero-title">
+                XON Fitness Club
+            </h2>
+      </div>
+  </div>;
+};
+
+export default Hero;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./navbar.css"
+import Logo from './logo';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className="main-common-style center border-bottom-main">
+      <div className="row-center-between container-common-style">
+        <Logo />
+        <div className="center gap-20">
+          <a className="link">Home</a>
+          <a className="link">Exercises</a>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
